b4a148e57e757c13a0eca4480dd5b53552ff5455
Sai Surya Sriramoju <64892218+Surya-Sriramoju@users.noreply.github.com>
2023-10-10 17:39:52 -0400

    Updated test.cpp
    after discussion, test cases updated

--- test/test.cpp
+++ test/test.cpp
@@ -2,14 +2,21 @@
 #include "pid.hpp"
 
 TEST(ComputeMethod, TEST) {
-  ASSERT_NEAR(120, PID(0.2, 0.1, 0.01, 0.1).runPid(120.0, 0.0), 0.5);
+  PID pid_cont = PID(0.2, 0.1, 0.01, 0.1);
+  double final_state = pid_cont.runPid(120.0, 0.0);
+  ASSERT_NEAR(120, final_state, 1);
 }
 
 TEST(ComputeMethod1, TEST2) {
-  ASSERT_NEAR(90, PID(0.2, 0.1, 0.01, 0.1).runPid(90.0, 10.0), 1);
-}
+  PID pid_cont = PID(0.2, 0.1, 0.01, 0.1);
+  double final_state = pid_cont.runPid(90.0, 10.0);
+  ASSERT_NEAR(90, final_state, 1);
+  }
 
 TEST(ComputeMethod2, TEST3) {
-  ASSERT_NEAR(200, PID(0.2, 0.1, 0.01, 0.1).runPid(200.0, 30.0), 0.5);
+  PID pid_cont = PID(0.2, 0.1, 0.01, 0.1);
+  double final_state = pid_cont.runPid(200.0, 30.0);
+  ASSERT_NEAR(200, final_state, 1);
 }
+
 
